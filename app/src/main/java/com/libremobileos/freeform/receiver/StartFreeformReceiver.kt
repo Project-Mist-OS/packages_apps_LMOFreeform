c999c5ee8e1504097a917b8b2eb8781ca57314d2
kenway214 <kenway214@outlook.com>
2025-07-19 20:25:50 +0530

    LMOFreeform: Improve landscape layout with left-side positioning
    - Adjusted default freeform window dimensions for better landscape usability.
    - In landscape mode, increased window width by 1.6x while maintaining aspect ratio.
    - Added logic to position the window on the left side in landscape.
    - Centered window in portrait
    
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- app/src/main/java/com/libremobileos/freeform/receiver/StartFreeformReceiver.kt
+++ app/src/main/java/com/libremobileos/freeform/receiver/StartFreeformReceiver.kt
@@ -34,10 +34,12 @@ class StartFreeformReceiver : BroadcastReceiver() {
                 val screenWidth = context.resources.displayMetrics.widthPixels
                 val screenHeight = context.resources.displayMetrics.heightPixels
                 val screenDensityDpi = context.resources.displayMetrics.densityDpi
-                val freeformWidth = sp.getInt("freeform_width", (screenWidth * 0.7).roundToInt())
+                
+                val freeformWidth = sp.getInt("freeform_width", (screenWidth * 0.6f).roundToInt())
                     .coerceAtMost(INITIAL_MAX_WIDTH)
-                val freeformHeight = sp.getInt("freeform_height", (screenHeight * 0.4).roundToInt())
+                val freeformHeight = sp.getInt("freeform_height", (screenHeight * 0.5f).roundToInt())
                     .coerceAtMost(INITIAL_MAX_HEIGHT)
+                
                 LMOFreeformServiceManager.createWindow(
                     packageName,
                     activityName,
